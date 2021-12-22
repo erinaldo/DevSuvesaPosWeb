36133ca4ce40fc3eca238b1e13f62ea6cf6eead7
Alberto Martinez Quesada <betomqcr@gmail.com>
2021-12-21 19:10:15 -0600

    Creacion clase Actividad

--- ApiSuvesaPos/DatosFE/Class/Actividades.cs
+++ ApiSuvesaPos/DatosFE/Class/Actividades.cs
@@ -3,10 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DatosFE.Models;
 
 namespace DatosFE.Class
 {
-    class Actividades
+    public class Actividades
     {
+        private FEContext entities;
+
+        public Actividades()
+        {
+            entities = new FEContext();
+        }
+
+        public int CrearActividad(ActividadEmpresa Actividad) // crear Actividad apartir de objeto de creacion
+        {
+            try
+            {
+                entities.ActividadEmpresas.Add(Actividad);
+                return entities.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public int BorrarActividad(int id) // borrar Actividad apartir del id Actividad
+
+
+        {
+            try
+            {
+                var p = entities.ActividadEmpresas.Find(id);
+                entities.Remove(p);
+                return entities.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public List<ActividadEmpresa> ObtenerActividades(string cedula) //obtener id cliente apartir de la cedula
+        {
+            try
+            {
+                var temp = from c in entities.ActividadEmpresas
+                           
+                           select c;
+                List<ActividadEmpresa> result = temp.ToList<ActividadEmpresa>();
+
+                if (result.Count>0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return result = null;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+
+
+
     }
 }
