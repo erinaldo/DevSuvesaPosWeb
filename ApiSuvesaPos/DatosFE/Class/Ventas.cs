b186e1f78265034636d451f3256ffcb044b21143
Alberto Martinez Quesada <betomqcr@gmail.com>
2021-12-28 14:24:21 -0600

    Creacion de metodos Obtner Clave, consecutivo y cambiar estado facturas en clase de Ventas

--- ApiSuvesaPos/DatosFE/Class/Ventas.cs
+++ ApiSuvesaPos/DatosFE/Class/Ventas.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DatosFE.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatosFE.Class
 {
@@ -76,5 +77,81 @@ namespace DatosFE.Class
                 throw ex;
             }
         }
+
+        public int EditarEstadoFactura (int id, string estado) // editar estado de Hacienda
+        {
+            try
+            {
+                var p = entities.Ventas.Find(id);
+                Venta viejo = p;
+                viejo.EstadoMh = estado;
+
+                entities.Entry(viejo).State = EntityState.Modified;
+
+                return entities.SaveChanges();
+
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string ObtenerClaveFactura(int id) // Retorna la clave de venta
+        {
+            try
+            {
+                var p = entities.Ventas.Find(id);
+                Venta viejo = p;
+
+                if (viejo != null)
+                {
+                    string clave = viejo.ClaveMh;
+                    return clave;
+                }
+                else
+                {
+                    return "No existe venta";
+                }
+
+
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string ObtenerNumeroConsecutivoFactura(int id) // Retorna la Numero Consecutivo de venta
+        {
+            try
+            {
+                var p = entities.Ventas.Find(id);
+                Venta viejo = p;
+
+                if (viejo != null)
+                {
+                    string consecutivo = viejo.ConsecutivoMh;
+                    return consecutivo;
+                }
+                else
+                {
+                    return "No existe venta";
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+
     }
 }
--- ApiSuvesaPos/DatosFE/Class/Vistas.cs
+++ ApiSuvesaPos/DatosFE/Class/Vistas.cs
@@ -7,7 +7,42 @@ using DatosFE.Models;
 
 namespace DatosFE.Class
 {
-    class Vistas
+    public class Vistas
     {
+        private FEContext entities;
+
+        public Vistas()
+        {
+            entities = new FEContext();
+        }
+
+        public List<ViewFactura> ObtenerFacturas() //obtener lista actividades 
+        {
+            try
+            {
+                var temp = from c in entities.ViewFacturas
+
+                           select c;
+                List<ViewFactura> result = temp.ToList<ViewFactura>();
+
+                if (result.Count > 0)
+                {
+                    
+                    return result;
+                }
+                else
+                {
+                    return result = null;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
     }
 }
