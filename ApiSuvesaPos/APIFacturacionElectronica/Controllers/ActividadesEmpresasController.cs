3dbf853084cee3d730b415195ec7c7327104b5aa
Alberto Martinez Quesada <betomqcr@gmail.com>
2021-12-28 17:42:06 -0600

    Creacion de Controlador en API FE

---
+++ ApiSuvesaPos/APIFacturacionElectronica/Controllers/ActividadesEmpresasController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using APIFacturacionElectronica.Models;
+
+namespace APIFacturacionElectronica.Controllers
+{
+    public class ActividadesEmpresasController : Controller
+    {
+        [HttpPost]
+        public IActionResult PostActividadesEmpresas( ActividadesEmpresas evidencia)
+        {
+            try
+            {
+                string resp = "Aqui va la parte de negocio"; //db.CrearEvidencia(evidencia); como este ejemplo
+
+                if (resp.Equals("1"))
+                {
+                    return CreatedAtRoute("DefaultApi", new {Actividad = evidencia.Actividad }, evidencia);
+                }
+                else if (resp.Equals("La persona no existe"))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw new Exception(resp);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestResult();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetObtenerActividadesEmpresa(int id)
+        {
+
+            var result = "lista que se obtiene desde negocio"; //this._context.Books.Include(c => c.Authors).SingleOrDefault(t => t.Id_Book == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            else if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return new NoContentResult();
+            }
+        }
+    }
+}
---
+++ ApiSuvesaPos/APIFacturacionElectronica/Models/ActividadesEmpresas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIFacturacionElectronica.Models
+{
+    public class ActividadesEmpresas
+    {
+        
+        public string Actividad { get; set; }
+    }
+}
