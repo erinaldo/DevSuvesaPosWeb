99526047016a38ed392f280165809aaf2542a204
Alberto Martinez Quesada <betomqcr@gmail.com>
2022-01-04 17:30:35 -0600

    Creacion de clases de Acciones,Actividades,AdelantosVentas,AdelantosVentasDetalles

---
+++ ApiSuvesaPos/Datos/Class/Actividades.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.Models;
+
+namespace Datos.Class
+{
+    class Actividades
+    {
+        private SeePOSContext entities;
+
+        public Actividades()
+        {
+            entities = new SeePOSContext();
+        }
+
+        public int CrearActividad(Actividade Actividad) // crear Actividad apartir de objeto de creacion
+        {
+            try
+            {
+                entities.Actividades.Add(Actividad);
+                return entities.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public int BorrarActividad(int id) // borrar Actividad apartir del id Actividad
+
+
+        {
+            try
+            {
+                var p = entities.Actividades.Find(id);
+                entities.Remove(p);
+                return entities.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public List<Actividades> ObtenerActividades() //obtener lista actividades 
+        {
+            try
+            {
+                var temp = from c in entities.Actividades
+
+                           select c;
+                List<Actividades> result = temp.ToList<Actividades>();
+
+                if (result.Count > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return result = null;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ ApiSuvesaPos/Datos/Class/AdelantosVentas.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datos.Class
+{
+    public class AdelantosVentas
+    {
+        SeePOSContext entities;
+
+        public AdelantosVentas()
+        {
+            entities = new SeePOSContext();
+        }
+
+        public int CrearAdelantoVenta(AdelantoVenta venta)
+        {
+            try
+            {
+                entities.AdelantoVentas.Add(venta);
+                return entities.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public int BorrarAbonoApartado(int id)
+
+
+        {
+            try
+            {
+                var p = entities.AdelantoVentas.Find(id);
+                entities.Remove(p);
+                return entities.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public List<AdelantoVenta> ObtenerAbonoApartado()
+        {
+            try
+            {
+                var temp = from c in entities.AdelantoVentas
+
+                           select c;
+                List<AdelantoVenta> result = temp.ToList<AdelantoVenta>();
+
+                if (result.Count > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return result = null;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<AdelantoVenta> ObtenerAbonoApartado(int id)
+        {
+            try
+            {
+                var temp = from c in entities.AdelantoVentas
+                           where c.Id == id
+                           select c;
+                List<AdelantoVenta> result = temp.ToList<AdelantoVenta>();
+
+                if (result.Count > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return result = null;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int EditarAbonoApartado(int id, AdelantoVenta abono)
+        {
+            try
+            {
+                var p = entities.AdelantoVentas.Find(id);
+                AdelantoVenta Nuevo = p;
+                Nuevo.CodCliente = abono.CodCliente;
+                Nuevo.NombreCliente = abono.NombreCliente;
+                Nuevo.Fecha = abono.Fecha;
+                Nuevo.Observaciones = abono.Observaciones;
+                Nuevo.CodMoneda = abono.CodMoneda;
+                Nuevo.IdSucursal = abono.IdSucursal;
+                //Nuevo.AbonoApartadosdetalles = abono.AbonoApartadosdetalle;
+
+                entities.Entry(Nuevo).State = EntityState.Modified;
+
+                return entities.SaveChanges();
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
