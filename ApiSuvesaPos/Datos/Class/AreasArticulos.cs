06aa47db2a0a6960448a6ba692367f84c39036fe
Alberto Martinez <betomqcr@gmail.com>
2022-01-07 21:12:19 -0600

    Mejora AreaArticulos

--- ApiSuvesaPos/Datos/Class/AreasArticulos.cs
+++ ApiSuvesaPos/Datos/Class/AreasArticulos.cs
@@ -3,10 +3,125 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datos.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Datos.Class
 {
-    class AreasArticulos
+    public class AreasArticulos
     {
+        SeePOSContext entities;
+
+        public AreasArticulos()
+        {
+            entities = new SeePOSContext();
+        }
+
+        public int CrearAreasArticulos(AreaArticulo area)
+        {
+            try
+            {
+                entities.AreaArticulos.Add(area);
+                return entities.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public int BorrarAreasArticulos(int id)
+
+
+        {
+            try
+            {
+                var p = entities.AreaArticulos.Find(id);
+                entities.Remove(p);
+                return entities.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public List<AreaArticulo> ObtenerAreasArticulos()
+        {
+            try
+            {
+                var temp = from c in entities.AreaArticulos
+
+                           select c;
+                List<AreaArticulo> result = temp.ToList<AreaArticulo>();
+
+                if (result.Count > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return result = null;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<AreaArticulo> ObtenerAreasArticulos(int id)
+        {
+            try
+            {
+                var temp = from c in entities.AreaArticulos
+                           where c.IdAreaArticulo == id
+                           select c;
+                List<AreaArticulo> result = temp.ToList<AreaArticulo>();
+
+                if (result.Count > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return result = null;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int EditarAreas(int id, Area abono)
+        {
+            try
+            {
+                var p = entities.Areas.Find(id);
+                Area Nuevo = p;
+                Nuevo.IdSucursal = abono.IdSucursal;
+                //Nuevo.AbonoApartadosdetalles = abono.AbonoApartadosdetalle;
+
+                entities.Entry(Nuevo).State = EntityState.Modified;
+
+                return entities.SaveChanges();
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
