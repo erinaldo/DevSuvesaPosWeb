d42cf8d2f3be6e8082a97e9952aaf622e23b18c7
Alberto Martínez Quesada <betomqcr@gmail.com>
2022-08-27 16:18:22 -0600

    Se crea controller Centros, pero sin implementar

---
+++ ApiSuvesaPos/ApiSuvesaPos/Controllers/CentrosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSuvesaPos.Controllers
+{
+    public class CentrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
