706cb899d68935c5e442af1bd676be8510262355
Carlos Pereira <carloscoto0103@gmail.com>
2022-09-03 14:52:26 -0600

    Clases de Helper para el manejo de las respuestas genericas

---
+++ ApiSuvesaPos/Datos/Helpers/Response.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Helpers
+{
+    public class Response
+    {
+        public Response()
+        {
+            Status = ResponseStatus.Success;
+            currentException = null;
+        }
+
+        public Response(string currentException)
+        {
+            Status = ResponseStatus.Faild;
+            this.currentException = currentException;
+        }
+
+        public Response(Exception currentException)
+        {
+            Status = ResponseStatus.Faild;
+            this.currentException = currentException.Message;
+        }
+
+        public ResponseStatus Status { get; set; }
+
+        public string currentException { get; set; }
+
+        public enum ResponseStatus
+        {
+            Success = 0,
+            Faild = 1
+        }
+
+    }
+}
---
+++ ApiSuvesaPos/Datos/Helpers/ResponseGeneric.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Helpers
+{
+    public class ResponseGeneric<T> : Response
+    {
+        public T Responses { get; set; }
+
+        public ResponseGeneric(T response) : base()
+        {
+            this.Responses = response;
+        }
+
+        public ResponseGeneric(Exception currentException) : base(currentException)
+        {
+            this.Responses = default(T);
+        }
+
+        public ResponseGeneric(string currentException) : base(currentException)
+        {
+            this.Responses = default(T);
+        }
+    }
+}
