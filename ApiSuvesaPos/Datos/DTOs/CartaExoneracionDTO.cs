0e43d414125b00b9be63c67fe51ca4c340121a09
Carlos Pereira <carloscoto0103@gmail.com>
2022-09-14 22:42:34 -0600

    Carta Exoneracion DTO

--- ApiSuvesaPos/Datos/DTOs/CartaExoneracionDTO.cs
+++ ApiSuvesaPos/Datos/DTOs/CartaExoneracionDTO.cs
@@ -4,6 +4,7 @@ namespace Datos.DTOs
 {
     public class CartaExoneracionDTO
     {
+        public string Cedula { get; set; }
         public int IdTipoExoneracion { get; set; }
         public string NumeroDocumento { get; set; }
         public DateTime FechaEmision { get; set; }
