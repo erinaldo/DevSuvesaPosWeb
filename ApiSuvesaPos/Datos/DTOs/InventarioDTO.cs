a32a4235c8b9eef91b79d112052bcf973b4a58d3
Carlos Pereira <carloscoto0103@gmail.com>
2022-10-08 15:40:08 -0600

    Clase InventarioDTO agregada

---
+++ ApiSuvesaPos/Datos/DTOs/InventarioDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DTOs
+{
+    public class InventarioDTO
+    {
+        public long Codigo { get; set; }
+        public long Cod_Articulo { get; set; }
+        public long Descripcion { get; set; }
+        public long Actualizado { get; set; }
+        public long SoloContado { get; set; }
+        public long CodPresentacion { get; set; }
+        public long Barras { get; set; }
+        public long barras2 { get; set; }
+        public long barras3 { get; set; }
+        public long SubFamilia { get; set; }
+        public long SubUbicacion { get; set; }
+        public long Proveedor { get; set; }
+        public long RebajaOtroArticulo { get; set; }
+        public long pantalla { get; set; }
+        public long CODCABYS { get; set; }
+
+    }
+}
